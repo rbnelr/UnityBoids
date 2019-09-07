5849d833acccc9f23d623c5117e1ae558f37fc65
Anon Ymous <rbnelr@gmx.de>
2019-09-07 17:03:42 +0200

    Revert "3d port"
    This reverts commit 279784b90ea047159950d53a3256afae6716b477.

--- Assets/BoidsSimulator.cs
+++ Assets/BoidsSimulator.cs
@@ -2,13 +2,12 @@
 using Unity.Mathematics;
 using static Unity.Mathematics.math;
 using System.Collections.Generic;
-using UnityEngine.Profiling;
 
 public class BoidsSimulator : MonoBehaviour {
 	
 	// http://www.cs.toronto.edu/~dt/siggraph97-course/cwr87/
 
-	public float3 Size = float3(100,100,100);
+	public float2 Size = float2(100,100);
 	public int BoidsCount = 100;
 	
 	public float SenseRadius = 25;
@@ -43,13 +42,13 @@ public class BoidsSimulator : MonoBehaviour {
 	Unity.Mathematics.Random rand = new Unity.Mathematics.Random(1);
 
 	struct Boid {
-		public float3 pos;
-		public float3 vel;
-		public float3 forward;
+		public float2 pos;
+		public float2 vel;
+		public float2 forward;
 		
-		public float3 avoid;
-		public float3 align;
-		public float3 center;
+		public float2 avoid;
+		public float2 align;
+		public float2 center;
 
 		public Color color;
 	};
@@ -63,9 +62,9 @@ public class BoidsSimulator : MonoBehaviour {
 
 	Boid spawn_boid () {
 		return new Boid {
-			pos = rand.NextFloat3() * Size,
-			vel = rand.NextFloat3Direction() * rand.NextFloat() * 0.2f * SpeedTarget,
-			forward = float3(0, 0, 1),
+			pos = rand.NextFloat2() * Size,
+			vel = rand.NextFloat2Direction() * rand.NextFloat() * 0.2f * SpeedTarget,
+			forward = float2(0, 1),
 
 			color = BoidColors.Evaluate(rand.NextFloat()),
 		};
@@ -96,7 +95,7 @@ public class BoidsSimulator : MonoBehaviour {
 			if (speed != 0)
 				boid.forward = boid.vel / speed;
 
-			float3 accel = 0;
+			float2 accel = 0;
 
 			accel +=  boid.forward * BoidAccel; // always accelerate forward
 			accel += -boid.forward * Drag(speed); // drag limits velocity
@@ -109,36 +108,34 @@ public class BoidsSimulator : MonoBehaviour {
 			boid.pos = boid.pos % Size;
 			boid.pos = select(boid.pos, boid.pos + Size, boid.pos < 0);
 
-			DrawBoid(boid);
+			InstanceBoid(boid);
 
 			boids[i] = boid;
 		}
 	}
 
-	float3 BoidLogic (int i, ref Boid boid, float speed) {
+	float2 BoidLogic (int i, ref Boid boid, float speed) {
 		
 		float SenseCosTheta = math.cos(math.radians(SenseAngle));
 
 		float radius = SenseRadius * BoidSize;
 
-		float3 avoidAvg = 0;
+		float2 avoidAvg = 0;
 		//float avoidCount = 0;
 
-		float3 alignAvg = 0;
+		float2 alignAvg = 0;
 		float alignCount = 0;
 
-		float3 centerAvg = 0;
+		float2 centerAvg = 0;
 		float centerCount = 0;
-		
-	Profiler.BeginSample("BoidLogic inner loop");
 
 		for (int j=0; j<boids.Count; ++j) {
 			if (i == j) continue;
 			var other = boids[j];
 			
-			float3 offs = other.pos - boid.pos;
+			float2 offs = other.pos - boid.pos;
 			float dist = length(offs);
-			float3 dir = dist != 0 ? offs / dist : 0;
+			float2 dir = dist != 0 ? offs / dist : 0;
 
 			if (dist > radius || dot(boid.forward, dir) < SenseCosTheta) continue;
 
@@ -161,17 +158,15 @@ public class BoidsSimulator : MonoBehaviour {
 			centerCount += 1;
 		}
 
-	Profiler.EndSample();
-
 		//if (avoidCount != 0) avoidAvg /= avoidCount;
 		if (alignCount != 0) alignAvg /= alignCount;
 		if (centerCount != 0) centerAvg /= centerCount;
 
 		float accelBudget = BoidManeuverBudget;
 
-		float3 avoid = avoidAvg * AvoidStrength * BoidManeuverBudget;
-		float3 align = alignAvg * AlignStrength * BoidManeuverBudget;
-		float3 center =  centerAvg * CenterStrength * BoidManeuverBudget;
+		float2 avoid = avoidAvg * AvoidStrength * BoidManeuverBudget;
+		float2 align = alignAvg * AlignStrength * BoidManeuverBudget;
+		float2 center =  centerAvg * CenterStrength * BoidManeuverBudget;
 
 		float avoidMag = min(length(avoid), accelBudget);
 		accelBudget -= avoidMag;
@@ -198,11 +193,9 @@ public class BoidsSimulator : MonoBehaviour {
 	List<Color> colors = new List<Color>();
 	List<int> triangles = new List<int>();
 	
-	Matrix4x4 Matrix (Boid boid) => Matrix4x4.TRS(boid.pos, Quaternion.LookRotation(boid.forward, float3(0,1,0)), float3(BoidSize));
+	Matrix4x4 Matrix (Boid boid) => Matrix4x4.TRS(float3(boid.pos, 0), Quaternion.LookRotation(float3(boid.forward, 0), float3(0,0,-1)), float3(BoidSize));
 
-	void DrawBoid (Boid boid) {
-	Profiler.BeginSample("DrawBoid");
-		
+	void InstanceBoid (Boid boid) {
 		var mat = Matrix(boid);
 
 		int triOffs = vertices.Count;
@@ -215,8 +208,6 @@ public class BoidsSimulator : MonoBehaviour {
 		for (int k=0; k<BoidMesh.triangles.Length; ++k) {
 			triangles.Add(triOffs + BoidMesh.triangles[k]);
 		}
-
-	Profiler.EndSample();
 	}
 
 	private void Update () {
@@ -224,10 +215,8 @@ public class BoidsSimulator : MonoBehaviour {
 		vertices.Clear();
 		colors.Clear();
 		triangles.Clear();
-		
-	Profiler.BeginSample("UpdateBoids");
+
 		UpdateBoids();
-	Profiler.EndSample();
 
 		proceduralMesh.SetVertices(vertices);
 		proceduralMesh.SetColors(colors);
@@ -235,16 +224,14 @@ public class BoidsSimulator : MonoBehaviour {
 
 		Graphics.DrawMesh(proceduralMesh, new Vector3(0,0,0), Quaternion.identity, BoidMaterial, 0);
 
-		//if (TrackBoid && boids.Count > 0)
-		//	Cam.transform.position = float3(boids[0].pos, Cam.transform.position.z);
+		if (TrackBoid && boids.Count > 0)
+			Cam.transform.position = float3(boids[0].pos, Cam.transform.position.z);
 	}
 
 	private void OnDrawGizmos () {
-	Profiler.BeginSample("OnDrawGizmos");
-
 		// Area outlines
 		Gizmos.color = Color.grey;
-		Gizmos.DrawWireCube(Size / 2, Size);
+		Gizmos.DrawWireCube(float3(Size / 2, 0), float3(Size, 1));
 
 		// Boid details
 		if (boids.Count > 0) {
@@ -258,11 +245,11 @@ public class BoidsSimulator : MonoBehaviour {
 			Gizmos.matrix = Matrix4x4.identity;
 
 			Gizmos.color = Color.red;
-			Gizmos.DrawRay(boid.pos, boid.avoid * 2);
+			Gizmos.DrawRay(float3(boid.pos, 0), float3(boid.avoid, 0) * 2);
 			Gizmos.color = Color.yellow;
-			Gizmos.DrawRay(boid.pos, boid.align * 2);
+			Gizmos.DrawRay(float3(boid.pos, 0), float3(boid.align, 0) * 2);
 			Gizmos.color = Color.cyan;
-			Gizmos.DrawRay(boid.pos, boid.center * 2);
+			Gizmos.DrawRay(float3(boid.pos, 0), float3(boid.center, 0) * 2);
 
 			if (true) {
 				float SenseCosTheta = math.cos(math.radians(SenseAngle));
@@ -272,18 +259,16 @@ public class BoidsSimulator : MonoBehaviour {
 					if (0 == j) continue;
 					var other = boids[j];
 
-					float3 offs = other.pos - boid.pos;
+					float2 offs = other.pos - boid.pos;
 					float dist = length(offs);
-					float3 dir = dist != 0 ? offs / dist : 0;
+					float2 dir = dist != 0 ? offs / dist : 0;
 
 					if (dist > SenseRadius * BoidSize || dot(boid.forward, dir) < SenseCosTheta) continue;
 					
 					Gizmos.color = dist < AvoidRadius * BoidSize ? Color.red : Color.green;
-					Gizmos.DrawLine(boid.pos, other.pos);
+					Gizmos.DrawLine(float3(boid.pos, 0), float3(other.pos, 0));
 				}
 			}
 		}
-
-	Profiler.EndSample();
 	}
 }
